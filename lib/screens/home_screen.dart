5bccfb9b181fc60fc3460f597ca4c9facdd8de67
jishnulal7 <Jishnulal9497@gmail.com>
2023-07-10 19:21:16 +0530

    Added Stream Builder

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_task/screens/camera_screen.dart';
 import 'package:firebase_task/screens/content_screen.dart';
+import 'package:firebase_task/screens/todo_card.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
@@ -12,6 +15,8 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final isdialOpen = ValueNotifier(false);
+  final Stream<QuerySnapshot> _stream =
+      FirebaseFirestore.instance.collection('To-do').snapshots();
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
@@ -32,19 +37,27 @@ class _HomeScreenState extends State<HomeScreen> {
         body: SingleChildScrollView(
           child: Column(
             children: [
-              ListView.builder(
-                itemCount: 4,
-                physics: const NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                itemBuilder: (context, index) => Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: 200,
-                    color: Colors.deepPurple,
-                  ),
-                ),
+              const SizedBox(
+                height: 20,
               ),
+              StreamBuilder(
+                  stream: _stream,
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                    return ListView.builder(
+                      itemCount: snapshot.data?.docs.length,
+                      physics: const NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        Map<String, dynamic> document =
+                            snapshot.data?.docs[index].data()
+                                as Map<String, dynamic>;
+                        return TodoCard(document: document);
+                      },
+                    );
+                  })
             ],
           ),
         ),
